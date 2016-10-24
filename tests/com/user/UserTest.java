85cd5565384d8f099666c98f8912649587168865
samlinz <lindqvist.samuel@gmail.com>
2016-10-25 01:34:43 +0300

    user class unit test

---
+++ tests/com/user/UserTest.java
@@ -0,0 +1,48 @@
+package com.user;
+
+import com.database.DatabaseConnector;
+import com.database.Queries;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * Created by samlinz on 25.10.2016.
+ */
+public class UserTest {
+
+    private DatabaseConnector conn;
+
+    @Test
+    public void userExists() throws Exception {
+        // test whether test user exists
+        Assert.assertTrue(User.userExists("unitTester"));
+    }
+
+    @Test
+    public void passwordMatch() throws Exception {
+        String pwd = com.database.StringCrypt.encrypt("kebab");
+        PreparedStatement sta = Queries.getQuery("getUser");
+        sta.setString(1, "unitTester");
+        ResultSet res = sta.executeQuery();
+        res.next();
+        Assert.assertTrue(res.getString("passwd").equals(pwd));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        conn.closeConnection();
+        conn.removeInstance();
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        // connect to local test db
+        conn = DatabaseConnector.getInstance();
+        conn.connect();
+    }
+}
